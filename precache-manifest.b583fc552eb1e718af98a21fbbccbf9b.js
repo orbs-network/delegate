d6e6e0923ddf4a2acfbd1ea543593a8d3fadb579
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-11-11 12:46:53 +0200

    Updates

---
+++ precache-manifest.b583fc552eb1e718af98a21fbbccbf9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f847ae6cf6ac46d3806720d53bedd171",
+    "url": "/delegate/index.html"
+  },
+  {
+    "revision": "0b99cd4311f103fb8084",
+    "url": "/delegate/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6e0d7b135f0d9247779d",
+    "url": "/delegate/static/js/2.cea41b77.chunk.js"
+  },
+  {
+    "revision": "0b99cd4311f103fb8084",
+    "url": "/delegate/static/js/main.fdb16ccd.chunk.js"
+  },
+  {
+    "revision": "d4c68c6c3c229e82d9aa",
+    "url": "/delegate/static/js/runtime-main.5b09243a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/delegate/static/media/logo.25bf045c.svg"
+  }
+]);
